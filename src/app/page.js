8db462f0850531e7c98cf612650edfbfc9ed6ecc
MTLY-1610 <larrouy.m@me.com>
2024-06-23 16:02:42 +0200

    hero/lander section no styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
 import Menu from "../components/Menu";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
+    
+    <>
     <Menu />
+    <Hero />
+    </>
     
   );
 }
---
+++ src/components/Hero.js
@@ -0,0 +1,22 @@
+// components/Menu.js
+
+import Link from 'next/link';
+import styles from './Hero.module.css';;
+
+const Menu = () => {
+  return (
+    <div className={styles.hero}> 
+      <section className={styles['hero-cont']} id="top">
+        <div className={styles['hero-pitch']} id="hero-cont">
+        </div>
+      </section>
+    </div>
+
+        
+    
+   
+
+  );
+};
+
+export default Hero;
